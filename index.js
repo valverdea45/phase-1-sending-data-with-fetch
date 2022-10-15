1b96bbb6b2b0431559195f7152e23240e86e578a
Adrian <adrianvalverde45@gmail.com>
2022-10-14 23:24:15 -0400

    commit tree 249d9f705abafc9fce65b5370723903ac474ae35

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(obj => document.body.append(obj.id))
+    .catch(function (message){
+        document.body.append(message)
+    })
+}
